7aa971a7920b525d2e99fc7a6e8223a1c1bafe18
nima7331 <nima.abbasnejad14@gmail.com>
2023-03-11 16:06:27 +0330

    add link git

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='fixer-demo',
       version='0.1',
       description='fixer service demo package',
-      url='#',
+      url='https://github.com/nima733/FixerPackage.git',
       outher='Nima',
       outher_email='nima.abbasnejad14@gmail.com',
       license='MIT',
