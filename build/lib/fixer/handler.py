5b4c6f6a09cc41e5d2876a7bbe8bd395fbefcdb2
nima7331 <nima.abbasnejad14@gmail.com>
2023-03-11 15:47:37 +0330

    Initial commit

---
+++ build/lib/fixer/handler.py
@@ -0,0 +1,2 @@
+def get_rate():
+    print("Getrate Touched")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fixer-demo',
+      version='0.1',
+      description='fixer service demo package',
+      url='#',
+      outher='Nima',
+      outher_email='nima.abbasnejad14@gmail.com',
+      license='MIT',
+      packages=['fixer'],
+      zip_safe=False)
